428101d54912174f6dd9e75a3b137ef391d49cfb
Akbermet05 <akbermetabdisatarova16@gmail.com>
2025-06-17 15:22:44 +0600

    .

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,15 +2,11 @@ import React from "react";
 import Footer from "./footer/Footer";
 import Header from "./header/Header";
 import scss from "./Layout.module.scss";
-import MainRoutes from "../../routes/MainRoutes";
-import SideBar from "../../home/SideBar";
 const Layout = () => {
   return (
     <div className={scss.layout}>
       <Header />
-      <main>
-        {/* <MainRoutes /> */}
-      </main>
+      <main>{/* <MainRoutes /> */}</main>
       <Footer />
     </div>
   );
